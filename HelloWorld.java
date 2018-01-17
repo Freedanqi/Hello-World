71f2e7636b3a86828b05eea8318efeb86d66cb46
Freedanqi <973785805@qq.com>
2018-01-17 08:29:27 +0000

    fix the file content

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,6 @@
 
-public class Tables {
+public class HelloWorld {
 	public static void main(String[] args){
-		int i,j;
-		for(i=1;i<=9;i++){
-			for(j=1;j<=i;j++){
-				System.out.print( j );
-				System.out.print("x");
-				System.out.print( i );
-				System.out.print("=");
-				System.out.print(j*i);
-				System.out.print("\t");
-			}
-			System.out.print("\n");
-		}
+		System.out.println("Hello World!");
 	}
 }
